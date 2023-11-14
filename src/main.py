1478470065f23f8674844fbbec1a8cbcc2e946b4
Billie Thompson <billie@billiecodes.com>
2023-11-14 09:55:54 +0100

    feat: add colours to the ui

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import subprocess
 from functools import lru_cache
 from tempfile import NamedTemporaryFile
 from typing import Annotated, Optional
-
+from rich.progress import track, Progress, SpinnerColumn
 import nltk
 import numpy as np
 import requests
@@ -19,90 +19,99 @@ cached_generate_audio = lru_cache(maxsize=1024)(generate_audio)
 
 
 def main(
-    source_text_file: Annotated[
-        str,
-        typer.Argument(
-            ...,
-            help="Text file to use as source for generation, could be a file path or url",
-        ),
-    ],
-    destination_file: Annotated[FileBinaryWrite, Argument(...)],
-    voice_prompt: Annotated[
-        Optional[str],
-        Option(
-            ...,
-            help="This will override any voice preset",
-        ),
-    ] = None,
-    voice_preset: Annotated[
-        Optional[str],
-        Option(
-            ...,
-            help="Voice preset, could be one of the built in ones or a path to a saved history",
-        ),
-    ] = "v2/en_speaker_9",
+        source_text_file: Annotated[
+            str,
+            typer.Argument(
+                ...,
+                help="Text file to use as source for generation, could be a file path or url",
+            ),
+        ],
+        destination_file: Annotated[FileBinaryWrite, Argument(...)],
+        voice_prompt: Annotated[
+            Optional[str],
+            Option(
+                ...,
+                help="This will override any voice preset",
+            ),
+        ] = None,
+        voice_preset: Annotated[
+            Optional[str],
+            Option(
+                ...,
+                help="Voice preset, could be one of the built in ones or a path to a saved history",
+            ),
+        ] = "v2/en_speaker_9",
 ) -> None:
-    preload_models(
-        text_use_gpu=torch.cuda.is_available(),
-        coarse_use_gpu=torch.cuda.is_available(),
-        fine_use_gpu=torch.cuda.is_available(),
-        codec_use_gpu=torch.cuda.is_available(),
-    )
-    nltk.download("punkt")
-    if source_text_file.startswith("http:") or source_text_file.startswith("https:"):
-        response = requests.get(source_text_file, timeout=60)
-        response.raise_for_status()
-        text_prompt = response.text.strip()
-    else:
-        with open(source_text_file, "r") as f:
-            text_prompt = f.read().strip()
-
-    sentences = pre_process_text(text_prompt)
-
-    silence = np.zeros(int(0.1 * SAMPLE_RATE))  # tenth of a second of silence
-
-    if voice_prompt is not None:
-        prompt_file = NamedTemporaryFile(suffix=".npz")
-        (full_generation, _) = generate_audio(
-            voice_prompt, silent=True, output_full=True
+    with Progress() as progress:
+        task = progress.add_task("Loading models", total=2)
+        preload_models(
+            text_use_gpu=torch.cuda.is_available(),
+            coarse_use_gpu=torch.cuda.is_available(),
+            fine_use_gpu=torch.cuda.is_available(),
+            codec_use_gpu=torch.cuda.is_available(),
         )
-        save_as_prompt(prompt_file.name, full_generation)
-        voice_preset = prompt_file.name
+        progress.update(task, advance=1)
+        nltk.download("punkt", quiet=True)
+        progress.update(task, advance=1)
 
-    pieces = []
-    with typer.progressbar(
-        sentences,
-        label="Generating audio",
-        show_eta=True,
-        show_percent=True,
-        show_pos=True,
-    ) as sentences_with_progress:
-        for sentence in sentences_with_progress:
+        if source_text_file.startswith("http:") or source_text_file.startswith("https:"):
+            task = progress.add_task("Downloading text", total=1)
+            response = requests.get(source_text_file, timeout=60)
+            response.raise_for_status()
+            text_prompt = response.text.strip()
+            progress.update(task, advance=1)
+        else:
+            with open(source_text_file, "r") as f:
+                text_prompt = f.read().strip()
+
+        task = progress.add_task("Preprocessing text", total=1)
+        sentences = pre_process_text(text_prompt)
+        progress.update(task, advance=1)
+
+        silence = np.zeros(int(0.1 * SAMPLE_RATE))  # tenth of a second of silence
+
+        if voice_prompt is not None:
+            task = progress.add_task("Generating voice from prompt", total=1)
+            prompt_file = NamedTemporaryFile(suffix=".npz")
+            (full_generation, _) = generate_audio(
+                voice_prompt, silent=True, output_full=True
+            )
+            save_as_prompt(prompt_file.name, full_generation)
+            voice_preset = prompt_file.name
+            progress.update(task, advance=1)
+
+        pieces = []
+
+        task = progress.add_task("Generating audio", total=len(sentences))
+        for sentence in sentences:
             audio_array = cached_generate_audio(
                 sentence, history_prompt=voice_preset, silent=True
             )
             pieces += [audio_array, silence.copy()]
+            progress.update(task, advance=1)
 
-    wav_path = f"{destination_file.name}.wav"
-    write_wav(wav_path, SAMPLE_RATE, np.concatenate(pieces).flatten())
+        wav_path = f"{destination_file.name}.wav"
+        write_wav(wav_path, SAMPLE_RATE, np.concatenate(pieces).flatten())
 
-    if os.path.exists(destination_file.name):
-        os.unlink(destination_file.name)
+        if os.path.exists(destination_file.name):
+            os.unlink(destination_file.name)
 
-    # note the escaping below
-    subprocess.run(
-        [  # noqa: S603,S607
-            "ffmpeg",
-            "-i",
-            shlex.quote(wav_path),
-            "-af",
-            "highpass=f=300,afftdn=nf=-20,dialoguenhance,lowpass=f=3000",
-            "-ar",
-            shlex.quote(str(SAMPLE_RATE)),
-            shlex.quote(destination_file.name),
-        ]
-    )
-    os.unlink(wav_path)
+        task = progress.add_task("Denoising", total=1)
+        # note the escaping below
+        subprocess.run(
+            [  # noqa: S603,S607
+                "ffmpeg",
+                "-i",
+                shlex.quote(wav_path),
+                "-af",
+                "highpass=f=300,afftdn=nf=-20,dialoguenhance,lowpass=f=3000",
+                "-ar",
+                shlex.quote(str(SAMPLE_RATE)),
+                shlex.quote(destination_file.name),
+            ]
+        )
+        os.unlink(wav_path)
+        progress.update(task, advance=1)
 
 
 def pre_process_text(text_prompt: str) -> list[str]:
